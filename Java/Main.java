21f9c0848681a421752f3473a7c2cf740d31a38c
Valentin Nussbaumer <83828188+ValentinNussbaumer@users.noreply.github.com>
2021-05-07 20:30:08 +0200

    Add files via upload

---
+++ Java/Main.java
@@ -0,0 +1,330 @@
+package com.company;
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args) {
+
+
+                System.out.println("Random password generator");
+                System.out.println("No data is stored");
+                System.out.println();
+                System.out.print("Your password: ");
+                Random r = new Random();
+                char c = (char)('a' + r.nextInt(27));
+                java.util.Random k = new java.util.Random();
+                int f = k.nextInt(9);
+
+                java.util.Random qc = new java.util.Random();
+                int qv = qc.nextInt(20);
+
+
+                Random q = new Random();
+                char w = (char)('a' + r.nextInt(27));
+
+                java.util.Random o = new java.util.Random();
+                int e = q.nextInt(9);
+
+
+                Random t = new Random();
+                char z = (char)('a' + r.nextInt(27));
+
+                java.util.Random  i = new java.util.Random();
+                int p = k.nextInt(9);
+
+
+                Random g = new Random();
+                char h = (char)('a' + r.nextInt(27));
+
+                java.util.Random j = new java.util.Random();
+                int l = q.nextInt(9);
+
+                Random d = new Random();
+                char m = (char)('a' + r.nextInt(27));
+
+                java.util.Random v = new java.util.Random();
+                int y = q.nextInt(9);
+
+
+
+                if (qv == 9) {
+                    Random qw = new Random();
+                    char qe = (char) ('a' + r.nextInt(27));
+                    System.out.print(qe);
+                }
+
+                if (qv == 10) {
+                    java.util.Random ql = new java.util.Random();
+                    int qp = q.nextInt(9);
+
+                    Random qw = new Random();
+                    char qe = (char) ('a' + r.nextInt(27));
+                    System.out.print(qp + qe);
+
+                }
+
+                if (qv == 11) {
+                    java.util.Random ql = new java.util.Random();
+                    int qp = q.nextInt(9);
+
+                    Random qw = new Random();
+                    char qe = (char) ('a' + r.nextInt(27));
+
+                    Random lj = new Random();
+                    char ap = (char) ('a' + r.nextInt(27));
+                    System.out.print(qp + qe + ap);
+                }
+
+                if (qv == 12) {
+                    java.util.Random qy = new java.util.Random();
+                    int lk = q.nextInt(9);
+
+                    java.util.Random ql = new java.util.Random();
+                    int qp = q.nextInt(9);
+
+                    Random qw = new Random();
+                    char qe = (char) ('a' + r.nextInt(27));
+
+                    Random lj = new Random();
+                    char ap = (char) ('a' + r.nextInt(27));
+                    System.out.print(lk + qp + qe + ap);
+
+                }
+
+                if (qv == 13) {
+                    java.util.Random qy = new java.util.Random();
+                    int lk = q.nextInt(9);
+
+                    java.util.Random ql = new java.util.Random();
+                    int qp = q.nextInt(9);
+
+                    Random qw = new Random();
+                    char qe = (char) ('a' + r.nextInt(27));
+
+                    Random lj = new Random();
+                    char ap = (char) ('a' + r.nextInt(27));
+
+                    Random lv = new Random();
+                    char ln = (char) ('a' + r.nextInt(27));
+                    System.out.print(lk + qp + qe + ap + ln);
+
+                }
+
+                if (qv == 14) {
+                    java.util.Random lm = new java.util.Random();
+                    int we = q.nextInt(9);
+                    java.util.Random qy = new java.util.Random();
+                    int lk = q.nextInt(9);
+
+                    java.util.Random ql = new java.util.Random();
+                    int qp = q.nextInt(9);
+
+                    Random qw = new Random();
+                    char qe = (char) ('a' + r.nextInt(27));
+
+                    Random lj = new Random();
+                    char ap = (char) ('a' + r.nextInt(27));
+
+                    Random lv = new Random();
+                    char ln = (char) ('a' + r.nextInt(27));
+                    System.out.print(we + lk + qp + qe + ap + ln);
+
+                }
+
+                if (qv == 15) {
+                    Random are = new Random();
+                    char not = (char) ('a' + r.nextInt(27));
+                    java.util.Random lm = new java.util.Random();
+                    int we = q.nextInt(9);
+                    java.util.Random qy = new java.util.Random();
+                    int lk = q.nextInt(9);
+                    java.util.Random ql = new java.util.Random();
+                    int qp = q.nextInt(9);
+                    Random qw = new Random();
+                    char qe = (char) ('a' + r.nextInt(27));
+                    Random lj = new Random();
+                    char ap = (char) ('a' + r.nextInt(27));
+
+                    Random lv = new Random();
+                    char ln = (char) ('a' + r.nextInt(27));
+                    System.out.print(not + we + lk + qp + qe + ap + ln);
+
+                }
+
+                if (qv == 16) {
+                    java.util.Random beau = new java.util.Random();
+                    int ti = q.nextInt(9);
+                    Random are = new Random();
+                    char not = (char) ('a' + r.nextInt(27));
+                    java.util.Random lm = new java.util.Random();
+                    int we = q.nextInt(9);
+                    java.util.Random qy = new java.util.Random();
+                    int lk = q.nextInt(9);
+
+                    java.util.Random ql = new java.util.Random();
+                    int qp = q.nextInt(9);
+
+                    Random qw = new Random();
+                    char qe = (char) ('a' + r.nextInt(27));
+
+                    Random lj = new Random();
+                    char ap = (char) ('a' + r.nextInt(27));
+
+                    Random lv = new Random();
+                    char ln = (char) ('a' + r.nextInt(27));
+                    System.out.print(ti + not + we + lk + qp + qe + ap + ln);
+                }
+
+                if (qv == 17) {
+                    java.util.Random ful = new java.util.Random();
+                    int but = q.nextInt(9);
+                    java.util.Random beau = new java.util.Random();
+                    int ti = q.nextInt(9);
+                    Random are = new Random();
+                    char not = (char) ('a' + r.nextInt(27));
+                    java.util.Random lm = new java.util.Random();
+                    int we = q.nextInt(9);
+                    java.util.Random qy = new java.util.Random();
+                    int lk = q.nextInt(9);
+
+                    java.util.Random ql = new java.util.Random();
+                    int qp = q.nextInt(9);
+
+                    Random qw = new Random();
+                    char qe = (char) ('a' + r.nextInt(27));
+
+                    Random lj = new Random();
+                    char ap = (char) ('a' + r.nextInt(27));
+
+                    Random lv = new Random();
+                    char ln = (char) ('a' + r.nextInt(27));
+                    System.out.print(but + ti + not + we + lk + qp + qe + ap + ln);
+                }
+
+                if (qv == 18) {
+                    java.util.Random the = new java.util.Random();
+                    int wor = q.nextInt(9);
+                    java.util.Random ful = new java.util.Random();
+                    int but = q.nextInt(9);
+                    java.util.Random beau = new java.util.Random();
+                    int ti = q.nextInt(9);
+                    Random are = new Random();
+                    char not = (char) ('a' + r.nextInt(27));
+                    java.util.Random lm = new java.util.Random();
+                    int we = q.nextInt(9);
+                    java.util.Random qy = new java.util.Random();
+                    int lk = q.nextInt(9);
+
+                    java.util.Random ql = new java.util.Random();
+                    int qp = q.nextInt(9);
+
+                    Random qw = new Random();
+                    char qe = (char) ('a' + r.nextInt(27));
+
+                    Random lj = new Random();
+                    char ap = (char) ('a' + r.nextInt(27));
+
+                    Random lv = new Random();
+                    char ln = (char) ('a' + r.nextInt(27));
+                    System.out.print(wor + but + ti + not + we + lk + qp + qe + ap + ln);
+                }
+
+
+                if (qv == 19) {
+                    java.util.Random ld = new java.util.Random();
+                    int is = q.nextInt(9);
+                    java.util.Random the = new java.util.Random();
+                    int wor = q.nextInt(9);
+                    java.util.Random ful = new java.util.Random();
+                    int but = q.nextInt(9);
+                    java.util.Random beau = new java.util.Random();
+                    int ti = q.nextInt(9);
+                    Random are = new Random();
+                    char not = (char) ('a' + r.nextInt(27));
+                    java.util.Random lm = new java.util.Random();
+                    int we = q.nextInt(9);
+                    java.util.Random qy = new java.util.Random();
+                    int lk = q.nextInt(9);
+
+                    java.util.Random ql = new java.util.Random();
+                    int qp = q.nextInt(9);
+
+                    Random qw = new Random();
+                    char qe = (char) ('a' + r.nextInt(27));
+
+                    Random lj = new Random();
+                    char ap = (char) ('a' + r.nextInt(27));
+
+                    Random lv = new Random();
+                    char ln = (char) ('a' + r.nextInt(27));
+                    System.out.print(is + wor + but + ti + not + we + lk + qp + qe + ap + ln);
+
+                }
+
+                if (qv == 20) {
+                    java.util.Random per = new java.util.Random();
+                    int fet = q.nextInt(9);
+
+                    java.util.Random ld = new java.util.Random();
+                    int is = q.nextInt(9);
+
+                    java.util.Random the = new java.util.Random();
+                    int wor = q.nextInt(9);
+
+                    java.util.Random ful = new java.util.Random();
+                    int but = q.nextInt(9);
+
+                    java.util.Random beau = new java.util.Random();
+                    int ti = q.nextInt(9);
+
+                    Random are = new Random();
+                    char not = (char) ('a' + r.nextInt(27));
+
+                    java.util.Random lm = new java.util.Random();
+                    int we = q.nextInt(9);
+
+                    java.util.Random qy = new java.util.Random();
+                    int lk = q.nextInt(9);
+
+
+                    java.util.Random ql = new java.util.Random();
+                    int qp = q.nextInt(9);
+
+                    Random qw = new Random();
+                    char qe = (char) ('a' + r.nextInt(27));
+
+
+                    Random lj = new Random();
+                    char ap = (char) ('a' + r.nextInt(27));
+
+
+                    Random lv = new Random();
+                    char ln = (char) ('a' + r.nextInt(27));
+
+
+                    System.out.print(fet + is + wor + but + ti + not + we + lk + qp + qe + ap + ln);
+                }
+                System.out.print(c);
+                System.out.print(f);
+                System.out.print(w);
+                System.out.print(e);
+                System.out.print(z);
+                System.out.print(p);
+                System.out.print(h);
+                System.out.print(l);
+                System.out.print(m);
+                System.out.print(y);
+                System.out.println();
+                System.out.println();
+
+                System.out.println("Made by Valentin Nussbaumer");
+                System.out.print("For bugs: nussbaumerv9@gmail.com");
+
+
+
+
+
+            }
+        }
+
+
+
